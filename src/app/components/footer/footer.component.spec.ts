e4eb67a022c90eb5eef335c772f10e2cd461e89b
JuanG <juaan8.gr@gmail.com>
2024-02-23 13:40:15 -0700

    feat(footer): Footer functionality was added to the project

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -1,23 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      declarations: [FooterComponent],
+    }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the FooterComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit getServices event when getTime is called', () => {
+    spyOn(component.getServices, 'emit');
+    component.getTime();
+    expect(component.getServices.emit).toHaveBeenCalled();
+  });
+
+  it('should update timeToCallFunc when updateTime is called', () => {
+    component.timeToCallFunc = 5;
+    component.updateTime();
+    expect(component.timeToCallFunc).toBe(4);
+  });
+
+  it('should call getTime when updateTime reaches 0', () => {
+    spyOn(component, 'getTime');
+    component.timeToCallFunc = 1;
+    component.updateTime();
+    expect(component.getTime).toHaveBeenCalled();
+  });
+
+  it('should clearInterval on ngOnDestroy', () => {
+    spyOn(window, 'clearInterval');
+    component.ngOnDestroy();
+    expect(window.clearInterval).toHaveBeenCalled();
   });
 });
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PotterServiceService } from 'src/app/services/potter-service.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  constructor(private potterServices: PotterServiceService) {}
 
+  ngOnInit(): void {
+    this.getServices();
+  }
+
+  getServices() {
+    this.potterServices.getAllServices();
+  }
 }
